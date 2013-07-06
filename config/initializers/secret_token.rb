722e2cc4c39d1423816d06cb5ad1cd540c709585
romy <rodudek@gmail.com>
2013-07-06 15:24:43 +0200

    initialcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birdytrainer::Application.config.secret_token = 'c2082054c62c7e25d9f2ee939f467d751fd4b5f34a60836121339ae037a01198a0727e7a372d577bfa8922465f7730647529c1daa94156c7c9066525022d8923'
